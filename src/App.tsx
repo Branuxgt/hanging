0f2cd34969c2c899fbd640720ef01e956f156b77
branuxgt <bmiranda_g@outlook.com>
2025-05-22 21:26:20 -0600

    mostrar la palabra

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,55 @@
-import {letters} from './helpers/letters'
-import './App.css'
-import { HangImage } from './components/HangImage'
+import { use, useState } from 'react';
+import {letters} from './helpers/letters';
+import './App.css';
+import { HangImage } from './components/HangImage';
 
 function App() {
- 
+
+  const [word] = useState('COMPUTADORA');
+  const [hiddenWord, setHiddenWord]= useState('_ '.repeat(word.length));
+  const [attempts,setAttempts ] = useState(0);
+
+  const checkLetter = (letter:string)=>{
+
+    if(!word.includes(letter)){
+      setAttempts( Math.min(attempts+1,9) );
+      return;
+    }
+
+    const hiddenWordArray = hiddenWord.split(' ');
+    console.log(hiddenWordArray);
+
+    for(let i = 0;i < word.length;i++){
+      if(word[i] === letter){
+        hiddenWordArray[i] = letter;
+      }
+    }
+    setHiddenWord(hiddenWordArray.join(' '))
+
+  }
+
   return(
   <div className='App'>
     
     
+    
     {/*Imagenes */}
-    <HangImage imageNumber={23}/>
+    <HangImage imageNumber={attempts}/>
     
     {/*Palabra oculta  */}
 
-    <h3>---------------------------</h3>
+    <h3> {hiddenWord} </h3>
     {/*Contador de intentos Imagenes */}
 
-    <h3>Intentos: 0</h3>
+    <h3>Intentos: {attempts}</h3>
     {/*Botones */}
 
     {
-      letters.map((letters)=> (
+      letters.map((letter)=> (
         <button
-          key={letters}>
-            {letters}
+          onClick={()=> checkLetter(letter)}
+          key={letter}>
+            {letter}
         </button>
       ))
     }
